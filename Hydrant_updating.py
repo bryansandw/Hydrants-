a8cbe8b739c461ee49314a0d86b50de321de768b
bryansandw <bryansandw@gmail.com>
2016-01-12 11:35:06 -0600

    Corrected line lengths

--- Hydrant_updating.py
+++ Hydrant_updating.py
@@ -1,98 +1,106 @@
-#################################################################################################################################
-# Name: Elizabeth Rentschlar                                                                                                    #
-# Purpose:                                                                                                                      #
-# Created: 7/15/15                                                                                                              #
-# Copyright: (c) City of Bryan                                                                                                  #
-# ArcGIS Version: 10.2.2                                                                                                        #
-# Python Version: 2.7                                                                                                           #
-#################################################################################################################################
+#############################################################################
+# Name: Elizabeth Rentschlar                                                #
+# Purpose:                                                                  #
+# Created: 7/15/15                                                          #
+# Copyright: (c) City of Bryan                                              #
+# ArcGIS Version: 10.2.2                                                    #
+# Python Version: 2.7                                                       #
+#############################################################################
+import os, glob
 import arcpy 
 
-# This is the folder that the hydrant pdfs that were converted to txt are located in
-folder = r'G:\GIS_PROJECTS\WATER_SERVICES\Tess\Hydrants\FakeHydrants'
-
-# The hydrant shapfile's location, or just name when it is open in the map.  
-# when using the python window in ArcMap you can drag the file into this location
-hydrant_shp = 'Hydrant_Copy'
+# This is the folder that the hydrant pdfs that were converted to txt are 
+# located in
+folder = r'C:\Users\erentschlar\Desktop\FakeHydrants'
 
 # file that the hydrant outputs is being written into 
-# currently appending the existing file this is intended to be a record of what hydrants have been processed
-out = r'G:\GIS_PROJECTS\WATER_SERVICES\Tess\Hydrants\Hydrants.txt'
+# currently appending the existing file this is intended to be a record of
+# what hydrants have been processed
+out = r'C:\Users\erentschlar\Desktop\Hydrants.txt'
 out1 = open(out, 'a')
 
-# These are the pdfs that will not be used for updating the shapefile
-no_go = r'G:\GIS_PROJECTS\WATER_SERVICES\Tess\Hydrants\Look_at_these_PDFs.txt'
-no_go1 = open(no_go, 'a')
-
-# These could be used if you would like to create a new file or overwrite an old file instead of appending to an existing file
-# header = 'Date	Flow Hydrant	Pilot Reading	Pitot Reading	Static	Static Hydrant	Residuals	Static_2	Grease1	Paint1	Type	Height	Manufacture Date \n'
+# These could be used if you would like to create a new file or overwrite
+# an old file instead of appending to an existing file
+# header = 'Date	Flow Hydrant	Pilot Reading	Pitot Reading' + \
+# '	Static	Static Hydrant	Residuals	Static_2	Grease1	Paint1' + \
+# '	Type	Height	Manufacture Date \n'
 # out1.write(header)
 
 # these are where all of the text files in the folder will be referenced
 txt_files = []
 
-# Find only the txt files in folder, this will keep the hydrant pdf forms from being processed unnecessarily 
-import os, glob
+# Find only the txt files in folder, this will keep the hydrant pdf forms
+# from being processed unnecessarily 
+
 path = folder
 for infile in glob.glob( os.path.join(path, '*.txt') ):
-        #print("current file is: " + infile) # to check that it is finding them
+        # to check that it is finding them
+        #print("current file is: " + infile) 
         txt_files.append(infile)   
 #print txt_files # to check that they were all in the list
 
-# Hydro_list will be a list of the hydrants sored as lists, this will allow the information to be accessed by the surch cursor later
+# Hydro_list will be a list of the hydrants sored as lists, this will allow
+# the information to be accessed by the surch cursor later
 hydro_list = []
-# The label_list will contain all of the hydrant labels that are in hydro_list, this will allow the code to correctly identify the index of the hydrant
+# The label_list will contain all of the hydrant labels that are in
+# hydro_list, this will allow the code to correctly identify the index of
+# the hydrant
 label_list = []
-# list1 is the same as the header information, it isn't really nessisary, but it helps keep the indexing in order
-list1 = ['Date','Flow Hydrant','Pilot Reading','Pitot Reading','Static','Static Hydrant','Residuals','Static_2','Grease1','Paint1','Type','Height','Manufacture Date']
+# list1 is the same as the header information, it isn't really nessisary,
+# but it helps keep the indexing in order
+list1 = ['Date','Flow Hydrant','Pilot Reading','Pitot Reading','Static',
+    'Static Hydrant','Residuals','Static_2','Grease1','Paint1','Type',
+    'Height','Manufacture Date']
 hydro_list.append(list1)
-# item is index 1, which is the 'Flow Hydrant' which is the same as the hydrant Label
+# item is index 1, which is the 'Flow Hydrant' which is the same as the
+# hydrant Label
 item = list1[1]
 label_list.append(item)
 
-# 
 for txt in txt_files:
     with open(txt, 'r') as f:
         next(f)
         for str in f:    
-            #print str
+            print str
             out1.write(str)
             list = str.split('\t')
             hydro_list.append(list)
-            # The labels should be in A##-###-AA format
+            # The labels sould be in A##-###-AA format
             hydro_label = list[1]
             if len(hydro_label) > 10:
-                no_go1.write(txt + '\n') #continue # need a files not processed txt file 
+                continue
             elif len(hydro_label) > 7:
                 label_list.append(hydro_label.upper())
             elif len(hydro_label) == 7:
                 hydro_label = hydro_label.upper() + "-FH"
                 label_list.append(hydro_label)
             else:
-                no_go1.write(txt+ '\n') #continue # need a files not processed txt file 
+                continue
 out1.close()
-no_go1.close()
 
-# May want some kind of out put that tell the user how many of the pdfs were greater than 10 or in the else category 
+# May want some kind of out put that tell the user how many of the pdfs were
+# greater than 10 or in the else category 
 
-#################################################################################################
-# This is where the ShapeFile will be processed with the data that was collected from the PDFs  #
-#################################################################################################
+#############################################################################
+# This is where the ShapeFile will be processed with the data that was      #
+# collected from the PDFs                                                   #
+#############################################################################
+# path to hydrants to be updated
+hydrants = arcpy.UpdateCursor('Hydrant_Copy', ["LABEL", "FLOW_DATE",
+     "PITOT_PSI", "PITOT_GPM", "PITOT_GPM", "STATIC_HYD", "RESID_PSI",
+     "ST_HYD_PSI", "Greased", "Painted"])  
 
-# This is a Class
-hydrants = arcpy.UpdateCursor(hydrant_shp, ["LABEL", "FLOW_DATE", "PITOT_PSI", "PITOT_GPM", "PITOT_GPM", "STATIC_HYD", "RESID_PSI", "ST_HYD_PSI", "Greased", "Painted"])  # path to hydrants to be updated
-
-# iterate through the features in the shapefile looking for hydrants that in the pdfs
+# iterate through the features in the shapefile looking for hydrants that in
+# the pdfs
 for hydrant in hydrants:
     if hydrant.LABEL in label_list:
         print hydrant.LABEL
         index = label_list.index(hydrant.LABEL)
         # print index # to check that the index was correct
         
-        # Defining the hydrant values that will be used to update the shapefile 
+        # Defining the hydrant values that will be used to update the
+        # shapefile 
         # need to check if they have null values or are wrong data type
-        
-        
         
         if hydro_list[index][2] != '':
             hydrant.PITOT_PSI = hydro_list[index][2]
@@ -101,7 +109,8 @@ for hydrant in hydrants:
         
         if hydro_list[index][3] != '':
             hydrant.PITOT_GPM = hydro_list[index][3]
-            hydrant.FLOW_DATE = hydro_list[index][0]      # Should only update Flow date if hydrant is flowed...	
+			# Should only update Flow date if hydrant is flowed...	
+            hydrant.FLOW_DATE = hydro_list[index][0]      
         else:
             pass
         
@@ -110,27 +119,25 @@ for hydrant in hydrants:
         else:
             pass
         
-		# The static label should be blank if the field is blank in the pdf, so else needs to pass a ' ' to actually over right the attribute
         if hydro_list[index][5] != '':
             shydro_label = hydro_list[index][5]
             if len(shydro_label) > 10:
-                pass
+                continue
             elif len(shydro_label) > 7:
                 hydrant.STATIC_HYD = shydro_label.upper()
             elif len(shydro_label) == 7:
                 shydro_label = shydro_label.upper() + "-FH"
                 hydrant.STATIC_HYD = shydro_label
             else:
-                pass
+                continue
+            #hydrant.STATIC_HYD = hydro_list[index][5]
         else:
-            hydrant.STATIC_HYD = ' '
+            pass
         
-		# The residual PSI should be 0 if the field is blank in the pdf
         if hydro_list[index][6] != '':
             hydrant.RESID_PSI = hydro_list[index][6]
         else:
-            hydrant.RESID_PSI = 0
-
+            pass
         
         if hydro_list[index][7] != '':
             hydrant.ST_HYD_PSI = hydro_list[index][7]
@@ -151,13 +158,18 @@ for hydrant in hydrants:
         else:
             pass
 		
-        # probably wont use these since they are only for new hydrants... what to do with new... 
-        #Model = hydro_list[index][10]
-        #HGHT = hydro_list[index][11]
-        #Manuf_Date = hydro_list[index][12]
+        
+        # probably wont use these since they are only for new hydrants... 
+		# what to do with new... 
+        Model = hydro_list[index][10]
+        HGHT = hydro_list[index][11]
+        Manuf_Date = hydro_list[index][12]
 
-        #print "date: " + hydrant.FLOW_DATE + "    PSI: " + hydrant.PITOT_PSI + "    GPM: " + hydrant.PITOT_GPM + "    Date Greased: " + hydrant.Greased + "    Date Painted: " + hydrant.Painted
+		
+#        print "date: %s    PSI: %s    GPM: %s    Date Greased: %s" + \
+#            "    Date Painted: %s" % (hydrant.FLOW_DATE, hydrant.PITOT_PSI,
+#    		    hydrant.PITOT_GPM, hydrant.Greased, hydrant.Painted)
         hydrants.updateRow(hydrant)
     else:
-        pass
+        continue
 del hydrant,hydrants
